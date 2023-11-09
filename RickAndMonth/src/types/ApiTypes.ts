e248a84055385b27639b6d317bb4e345224d927e
JRaamos <jhonyramos46@gmail.com>
2023-11-09 19:50:41 -0300

    feat: configuração inicial do projeto

---
+++ RickAndMonth/src/types/ApiTypes.ts
@@ -0,0 +1,34 @@
+// Definição de tipos para a origem e localização dos personagens
+interface Location {
+  name: string;
+  url: string;
+}
+
+// Definição de tipo para um personagem
+interface Character {
+  id: number;
+  name: string;
+  status: 'Alive' | 'Dead' | 'unknown'; 
+  species: string;
+  type: string;
+  gender: 'Female' | 'Male' | 'Genderless' | 'unknown'; 
+  origin: Location;
+  location: Location;
+  image: string;
+  episode: string[];
+  url: string;
+  created: string; 
+}
+
+export interface ApiResponse {
+  results: Character[];
+}
+
+// // Exemplo de uso:
+// const fetchCharacters = async (): Promise<ApiResponse> => {
+//   const response = await fetch('https://rickandmortyapi.com/api/character');
+//   const data: ApiResponse = await response.json();
+//   return data;
+// };
+
+// Agora, quando você usar fetchCharacters, o TypeScript saberá o tipo de retorno
